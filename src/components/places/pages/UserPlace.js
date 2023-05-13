7a44a072bba5dc36fe6e0bdd9eebcd3920cb8098
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-13 20:35:40 +0530

    Get place by user id using api

--- src/components/places/pages/UserPlace.js
+++ src/components/places/pages/UserPlace.js
@@ -1,40 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 
-function UserPlace(props) {
-  const userID = useParams().userID;
-  const demoPlaces = [
-    {
-      id: "p1",
-      title: "Red Fort",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm8LcGbb_GcSH4dmWuiHPJY0C-U7KZ0-QXMA&usqp=CAU      ",
-      descrition:
-        "17th-century Mughal fortress built in towering red sandstone, now a complex of museums.",
-      address:
-        "Netaji Subhash Marg, Lal Qila, Chandni Chowk, New Delhi, Delhi 110006",
-      userID: "1",
-    },
-    {
-      id: "p2",
-      title: "Taj Mehel",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE0YT7qpsWzJh6MEbQ39K_Zuaqw2_FEEVSdQ&usqp=CAU",
-      descrition:
-        "17th-century, Mughal-style, marble mausoleum with minarets, a mosque & famously symmetrical gardens.",
-      address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001",
-      userID: "2",
-    },
-  ];
+import Loading from "../../shared/components/Loading";
+import { useHttpClient } from "../../../hooks/fetchCall";
+function UserPlace() {
+  const [placeOfUser, setPlaceOfUser] = useState();
 
-  const userIdPlaces = demoPlaces.filter((place) => {
-    return place.userID === userID;
-  });
+  const { loading, sendRequest } = useHttpClient();
+
+  const userID = useParams().userID;
+
+  useEffect(() => {
+    const getUserPlaces = async () => {
+      try {
+        const response = await sendRequest(`api/places/users/${userID}`);
+        const places = await response;
+        setPlaceOfUser(places);
+      } catch (err) {}
+    };
+    getUserPlaces();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="container">
-      <PlaceList places={userIdPlaces} />
+      {loading && !placeOfUser && <Loading />}
+      {!loading && placeOfUser && <PlaceList places={placeOfUser} />}
     </div>
   );
 }
