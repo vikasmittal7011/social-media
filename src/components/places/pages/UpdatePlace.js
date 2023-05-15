a89d65c6f5d293d4a31dde8611c446bfe23b3116
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-15 20:28:58 +0530

    Fetch edit place detail & set values in frontend

--- src/components/places/pages/UpdatePlace.js
+++ src/components/places/pages/UpdatePlace.js
@@ -6,38 +6,18 @@ import { useCallback } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useHttpClient } from "../../../hooks/fetchCall";
 
 function UpdatePlace() {
-  const demoPlaces = [
-    {
-      id: "p1",
-      title: "Red Fort",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm8LcGbb_GcSH4dmWuiHPJY0C-U7KZ0-QXMA&usqp=CAU      ",
-      descrition:
-        "17th-century Mughal fortress built in towering red sandstone, now a complex of museums.",
-      address:
-        "Netaji Subhash Marg, Lal Qila, Chandni Chowk, New Delhi, Delhi 110006",
-      userID: "1",
-    },
-    {
-      id: "p2",
-      title: "Taj Mehel",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE0YT7qpsWzJh6MEbQ39K_Zuaqw2_FEEVSdQ&usqp=CAU",
-      descrition:
-        "17th-century, Mughal-style, marble mausoleum with minarets, a mosque & famously symmetrical gardens.",
-      address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001",
-      userID: "2",
-    },
-  ];
+  const { sendRequest } = useHttpClient();
+
   const placeId = useParams().placeId;
 
-  const editPlace = demoPlaces.find((place) => place.id === placeId);
+  const [placeForUpdate, setPLaceForUpdate] = useState();
 
-  const [updatePlace, setUpdatePlace] = useState(editPlace);
+  const [updatePlace, setUpdatePlace] = useState(placeForUpdate);
 
-  const [isFormValid, setIsFormvalid] = useState(true);
+  const [isFormvalid, setIsFormvalid] = useState(false);
 
   const navigate = useNavigate();
 
@@ -52,7 +32,7 @@ function UpdatePlace() {
       checkValid(title, updatePlace.descrition);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [editPlace]
+    [updatePlace]
   );
 
   const handleDesChange = useCallback(
@@ -64,11 +44,11 @@ function UpdatePlace() {
       checkValid(updatePlace.title, des);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [editPlace]
+    [updatePlace]
   );
 
   const checkValid = (title, des) => {
-    if (title > 3 && des > 5) {
+    if (title.length >= 3 && des.length >= 5) {
       setIsFormvalid(true);
     } else {
       setIsFormvalid(false);
@@ -76,37 +56,56 @@ function UpdatePlace() {
   };
 
   useEffect(() => {
-    if (!userLogin) {
-      navigate("/");
-    }
-  }, [userLogin, navigate]);
+    const getPlace = async () => {
+      const place = await sendRequest(`api/places/${placeId}`);
+      setPLaceForUpdate(place);
+      setUpdatePlace(place);
+    };
+    getPlace();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  const onUpdatePlaceHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  if (!userLogin) {
+    navigate("/");
+  }
   return (
-    <div className="container" style={{ width: "70vw" }}>
-      <h2 className="text-center">Update Place Here!!! </h2>
-      <form>
-        <Input
-          title="Name of Place"
-          id="title"
-          type="text"
-          placeholder="Enter name of place"
-          required={true}
-          value={editPlace.title}
-          onInput={handleTitleChange}
-        />
-        <Input
-          title="Descrition"
-          id="descrition"
-          type="text"
-          placeholder="Write somthing about it..."
-          required={true}
-          value={editPlace.descrition}
-          textarea={true}
-          onInput={handleDesChange}
-        />
-        <FormButton name="Update Place" type="primary" activate={isFormValid} />
-      </form>
-    </div>
+    placeForUpdate && (
+      <div className="container" style={{ width: "70vw" }}>
+        <h2 className="text-center">Update Place Here!!! </h2>
+        <form onSubmit={onUpdatePlaceHandler}>
+          <Input
+            title="Name of Place"
+            id="title"
+            type="text"
+            placeholder="Enter name of place"
+            required={true}
+            value={placeForUpdate.title}
+            onInput={handleTitleChange}
+            length={3}
+          />
+          <Input
+            title="Descrition"
+            id="descrition"
+            type="text"
+            placeholder="Write somthing about it..."
+            required={true}
+            value={placeForUpdate.descrition}
+            textarea={true}
+            onInput={handleDesChange}
+            length={5}
+          />
+          <FormButton
+            name="Update Place"
+            type="primary"
+            activate={isFormvalid}
+          />
+        </form>
+      </div>
+    )
   );
 }
 
