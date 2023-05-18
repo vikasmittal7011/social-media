0924e22d72c63ca6974ecab1f856892868819255
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-18 18:44:05 +0530

    Add lazy to geting routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import { useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Users from "./components/users/pages/Users";
-import Navbar from "./components/shared/components/Navbar";
-import About from "./components/shared/components/About";
-import UserPlace from "./components/places/pages/UserPlace";
-import Alert from "./components/shared/components/Alert";
-import { useSelector } from "react-redux";
-import AddPlaces from "./components/places/pages/AddPlaces";
-import UpdatePlace from "./components/places/pages/UpdatePlace";
-import Auth from "./components/users/pages/Auth";
-import Register from "./components/users/pages/Register";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
+
 import { actionCreators } from "./state";
-// import { useNavigate } from "react-router-dom";
+import Loading from "./components/shared/components/Loading";
+const Users = lazy(() => import("./components/users/pages/Users"));
+const UpdatePlace = lazy(() => import("./components/places/pages/UpdatePlace"));
+const Auth = lazy(() => import("./components/users/pages/Auth"));
+const UserPlace = lazy(() => import("./components/places/pages/UserPlace"));
+const Alert = lazy(() => import("./components/shared/components/Alert"));
+const Navbar = lazy(() => import("./components/shared/components/Navbar"));
+const About = lazy(() => import("./components/shared/components/About"));
+const Register = lazy(() => import("./components/users/pages/Register"));
+const AddPlaces = lazy(() => import("./components/places/pages/AddPlaces"));
 
 const App = () => {
   const alert = useSelector((state) => state.alert);
   const dispatch = useDispatch();
   const { updateUserLogin } = bindActionCreators(actionCreators, dispatch);
-  // const navigate = useNavigate();
   const userData = JSON.parse(localStorage.getItem("userData"));
   useEffect(() => {
     if (
@@ -50,19 +49,21 @@ const App = () => {
       <Navbar />
       <div>
         <main style={{ marginTop: "5rem" }}>
-          <div style={{ height: "1.5rem" }}>
-            <Alert alert={alert} />
-          </div>
-          <Routes>
-            <Route exact path="/" element={<Users />} />
-            <Route exact path="about" element={<About />} />
-            <Route exact path="/:userID/places" element={<UserPlace />} />
-            <Route exact path="/places/addPlace" element={<AddPlaces />} />
-            <Route exact path="/places/:placeId" element={<UpdatePlace />} />
-            <Route exact path="/login" element={<Auth />} />
-            <Route exact path="/register" element={<Register />} />
-            <Route path="*" element={<Users />} />
-          </Routes>
+          <Suspense fallback={<Loading />}>
+            <div style={{ height: "1.5rem" }}>
+              <Alert alert={alert} />
+            </div>
+            <Routes>
+              <Route exact path="/" element={<Users />} />
+              <Route exact path="about" element={<About />} />
+              <Route exact path="/:userID/places" element={<UserPlace />} />
+              <Route exact path="/places/addPlace" element={<AddPlaces />} />
+              <Route exact path="/places/:placeId" element={<UpdatePlace />} />
+              <Route exact path="/login" element={<Auth />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route path="*" element={<Users />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </BrowserRouter>
